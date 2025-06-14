cc9648a0b8e137ee41880b0628cdfc453d080764
chiranjibi-kusari <chiranjibikusari1@gmail.com>
2025-06-14 15:46:36 +0545

    mongobd connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import dotenv from "dotenv"
+import mongoose from 'mongoose'
 const app=express()
-
+const PORT=process.env.PORT
 dotenv.config()
 
 
 // mongodb connection
-const PORT=process.env.PORT
+  try {
+  await mongoose.connect(process.env.MONGODB_URL)
+  console.log("Mongodb connection successfully");
+  
+} catch (error) {
+  console.log("mongodb failed",error);
+  
+}
+
 app.get('/',(req,res)=>{
   res.send("hello")
 })
